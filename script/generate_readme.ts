c015ad6e1d5b8fd17c3ae1688ef6108482d53314
alexzhang1030 <1642114555@qq.com>
2022-06-23 22:36:10 +0800

    chore: update

--- script/generate_readme.ts
+++ script/generate_readme.ts
@@ -9,7 +9,7 @@ function write(type: string, index: number) {
 }
 
 function generateContent(filesNames: string[], type: string, index: number) {
-  const prefix = `## part ${index} ${type} \n\r`
+  const prefix = `### part ${index} ${type} \n`
   const list = filesNames.map((item) => {
     const name = /_(.*?).md/.exec(item)![1]
     return `- [${name}](${envs!.VALID_DOCS_NAME}/0${index}_${name}.md)`
@@ -22,7 +22,7 @@ function main() {
     let final_content = envs!.DOCS_PREFIX
     types.forEach((t, i) => {
       const content = write(t, i + 1)
-      final_content += `\n\r ${content}`
+      final_content += `\n\r ${content}\n`
     })
     writeFileSync(resolve(root, envs!.README_FILE_NAME), final_content)
     // eslint-disable-next-line no-console
